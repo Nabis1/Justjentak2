0520488ead4baeaa07b202fd5ef0d20bc1b30813
Nabis <r.cernil332@gmail.com>
2023-12-15 13:01:19 +0100

    Add project files.

---
+++ Justjentak2/Hruska.cs
@@ -0,0 +1,25 @@
+﻿namespace Justjentak2
+{
+    public class Hruska
+    {
+        public Hruska(string barva, double velikost, int id)
+            
+        { 
+            Barva = barva;
+            Velikost = velikost;
+            Id = id;
+        }
+        
+        
+        public string Barva = "Neznama";
+        public double Velikost = 100.0;
+        public int Id = 0;
+
+        public void Vlastnosti()
+        {
+            Console.WriteLine( $"Jsem hruska Zdenek {Barva}," +
+                $" velikost mam  {Velikost}" + 
+                $" a moje seriove cislo je {Id}");
+        }
+    }
+}
---
+++ Justjentak2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Justjentak2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hruska zdenek = new Hruska("Zluta", 100.0, 1 );
+            Hruska josef = new Hruska("Zelena", 72.0, 45);
+            zdenek.Vlastnosti();
+            josef.Vlastnosti();
+
+           // zdenek.Barva = "Zluta";
+           // zdenek.Velikost = 3;
+           // zdenek.Id = 360;
+
+           // zdenek.Vlastnosti();
+        }
+
+
+
+
+
+
+
+
+        
+
+
+    }
+}
+    
