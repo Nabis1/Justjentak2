0a944f2931622a0724461d8a9541e60034c6323c
Nabis <r.cernil332@gmail.com>
2023-12-15 13:28:03 +0100

    pridal jsem Jmena a defaultni konstruktor.

--- Justjentak2/Hruska.cs
+++ Justjentak2/Hruska.cs
@@ -2,22 +2,28 @@
 {
     public class Hruska
     {
-        public Hruska(string barva, double velikost, int id)
+        public Hruska(string barva, double velikost, int id, string jmeno)
             
         { 
             Barva = barva;
             Velikost = velikost;
             Id = id;
+            Jmeno = jmeno;
         }
-        
-        
+        public Hruska()
+        {
+
+        }
+
+
+        public string Jmeno = "Anonym";
         public string Barva = "Neznama";
         public double Velikost = 100.0;
         public int Id = 0;
 
         public void Vlastnosti()
         {
-            Console.WriteLine( $"Jsem hruska Zdenek {Barva}," +
+            Console.WriteLine( $"Jsem hruska,jmenuji se {Jmeno}, moje barva je {Barva}," +
                 $" velikost mam  {Velikost}" + 
                 $" a moje seriove cislo je {Id}");
         }
--- Justjentak2/Program.cs
+++ Justjentak2/Program.cs
@@ -6,16 +6,18 @@ namespace Justjentak2
     {
         static void Main(string[] args)
         {
-            Hruska zdenek = new Hruska("Zluta", 100.0, 1 );
-            Hruska josef = new Hruska("Zelena", 72.0, 45);
+            Hruska zdenek = new Hruska("Zluta", 100.0,1,"Zdenek" );
+            Hruska josef = new Hruska("Zelena", 72.0, 45, "Josef");
+            Hruska karel = new Hruska();
             zdenek.Vlastnosti();
             josef.Vlastnosti();
+            karel.Vlastnosti();
+            // tyto vlastnosti napise az metoda pod 
+            zdenek.Barva = "Zluta";
+            zdenek.Velikost = 80.7;
+            zdenek.Id = 360;
 
-           // zdenek.Barva = "Zluta";
-           // zdenek.Velikost = 3;
-           // zdenek.Id = 360;
-
-           // zdenek.Vlastnosti();
+            zdenek.Vlastnosti();
         }
 
 
